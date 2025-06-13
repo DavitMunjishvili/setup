0d38b3a43c3d776f566697eb6397c8e54a4243a1
Data Munjishvili <drummerthree@gmail.com>
2025-06-13 18:37:11 +0400

    install script for homebrew

--- scripts/macos/main.fish
+++ scripts/macos/main.fish
@@ -9,6 +9,15 @@ for preference_script in (ls ./scripts/macos/preferences/*.fish)
 end
 
 
+if not type brew > /dev/null
+    echo "--- Installing Homebrew ---"
+    run_or_echo "/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'"
+else
+    echo "--- Homebrew is already installed ---"
+end
+
+
+
 echo "--- Installing apps ---"
 for app_script in (ls ./apps/*.fish)
     echo "--- Running $app_script ---"
