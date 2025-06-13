f7fd40f4a79442af801ee1062fb535344037bd64
Data Munjishvili <drummerthree@gmail.com>
2025-06-13 18:49:48 +0400

    added btop install script

---
+++ apps/btop.fish
@@ -0,0 +1,21 @@
+#!/usr/bin/env fish
+
+set platform $argv[1]
+set app "btop"
+
+source ./utils/helpers.fish
+
+if is_installed $app $platform
+    echo "$app is already installed, skipping."
+    exit 0
+end
+
+switch $platform
+    case macos
+        run_or_echo "brew install $app"
+    case fedora
+        run_or_echo "sudo dnf install $app"
+    case '*'
+        echo "Unsupported platform: $platform for $app"
+        exit 1
+end
