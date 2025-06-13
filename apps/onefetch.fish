13d339a3df84581633c6a62cfd53a12d0932315b
Data Munjishvili <drummerthree@gmail.com>
2025-06-13 19:24:01 +0400

    added onefetch install script

---
+++ apps/onefetch.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+set platform $argv[1]
+set app "onefetch"
+
+source ./utils/helpers.fish
+
+if is_installed $app $platform
+    echo "$app is already installed, skipping."
+    exit 0
+end
+
+switch $platform
+    case macos
+        run_or_echo "brew install $app"
+    case arch
+        run_or_echo "sudo pacman -S $app"
+    case debian
+        run_or_echo "sudo apt install $app"
+    case fedora
+        run_or_echo "sudo dnf install $app"
+    case '*'
+        echo "Unsupported platform: $platform for $app"
+        exit 1
+end
