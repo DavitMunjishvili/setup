389a7315fb7247ee9d2ff350d3bd2e9ef78c661e
Data Munjishvili <drummerthree@gmail.com>
2025-06-13 18:38:24 +0400

    prettier logs

--- scripts/macos/main.fish
+++ scripts/macos/main.fish
@@ -2,7 +2,7 @@
 
 echo "running macos-specific setup..."
 
-echo "--- Settings macos preferences ---"
+echo "Settings macos preferences:"
 for preference_script in (ls ./scripts/macos/preferences/*.fish)
     echo "--- Running $preference_script ---"
     fish $preference_script
@@ -10,7 +10,7 @@ end
 
 
 if not type brew > /dev/null
-    echo "--- Installing Homebrew ---"
+    echo "Installing Homebrew:"
     run_or_echo "/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'"
 else
     echo "--- Homebrew is already installed ---"
@@ -19,14 +19,14 @@ end
 
 
 if not type git > /dev/null
-    echo "--- Installing Git ---"
+    echo "Installing Git:"
     run_or_echo "brew install git"
 else
     echo "--- Git is already installed ---"
 end
 
 
-echo "--- Installing apps ---"
+echo "Installing apps:"
 for app_script in (ls ./apps/*.fish)
     echo "--- Running $app_script ---"
     fish $app_script macos
